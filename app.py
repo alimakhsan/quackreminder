209b03b73f7e8666fcac5ee1908257f71187a431
Ahmad Alim Akhsan <Ahmad Alim Akhsan>
2017-03-16 08:25:06 +0700

    Replies

--- app.py
+++ app.py
@@ -30,47 +30,46 @@ def webhook():
   
     if data["object"] == "page":
         for entry in data["entry"]:
-            for messaging_event in entry["messaging"]:
 
-                payload = request.get_data()
-                sender, message = messaging_events(payload)
+            payload = request.get_data()
+            sender, message = messaging_events(payload)
 
-                #greetings
-                #if any(greeting() == message.lower() for greeting in greetings):
-                #    send_text_message(sender, "Hi there!")
-                if message.lower() == 'hi':
-                    send_text_message(sender, "hi tooooooooo")
+            #greetings
+            #if any(greeting() == message.lower() for greeting in greetings):
+            #    send_text_message(sender, "Hi there!")
+            if message.lower() == 'hi':
+                send_text_message(sender, "hi tooooooooo")
 
-                elif message == "Hola":
-                    send_text_message(sender, "hola tooooooooo")
+            elif message == "Hola":
+                send_text_message(sender, "hola tooooooooo")
 
-                elif any(greeting() == message.lower() for greeting in greetings):
-                    send_text_message(sender, "Hi there!")
+            elif any(greeting() == message.lower() for greeting in greetings):
+                send_text_message(sender, "Hi there!")
 
-                #help
-                elif message.lower() == 'help':
-                    send_button_template_message(sender, "What can I help you?",
-                        [
-                            generate_button(
-                                "show me examples"
-                                ),
-                            generate_button(
-                                "show my reminders"
-                                )
-                        ])
-                #show examples
-                #handle task 1    
-                #handle task 2
-                #handle task 3
-                #handle task 4
-                #handle task 5
-                #handle task 6
-                #handle task 7
-                #handle task 8
-                #handle task 9
-                #handle task 10
-                else:
-                    send_text_message(sender, "Sorry I'm just a little ducky")
+            #help
+            elif message.lower() == 'help':
+                send_button_template_message(sender, "What can I help you?",
+                    [
+                        generate_button(
+                            "show me examples"
+                            ),
+                        generate_button(
+                            "show my reminders"
+                            )
+                    ])
+            #show examples
+            #handle task 1    
+            #handle task 2
+            #handle task 3
+            #handle task 4
+            #handle task 5
+            #handle task 6
+            #handle task 7
+            #handle task 8
+            #handle task 9
+            #handle task 10
+            else:
+                send_text_message(sender, "Sorry I'm just a little ducky")
         
     return "ok", 200
 
